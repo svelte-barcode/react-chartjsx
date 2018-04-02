a18436529546532818c541f3390849533987f38f
Bunlong <bunlong.van@gmail.com>
2018-04-02 14:05:54 +0700

    add react-chartjsx

--- index.js
+++
@@ -1,3 +0,0 @@
-exports.printMsg = function() {
-  console.log("react-chartjsX Under development!");
-}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Chart extends React.Component {
+  render() {
+    return (
+      <div>Chart!</div>
+    );
+  }
+}
+
+export default Chart;
